0f78f5b83ece708426ed8af1873c8f9d50173337
John Mburu <johnnkonge2020@gmail.com>
2025-10-08 10:59:32 +0300

    compiled and ran the first program

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,16 @@
-//first hello world program
+// //first hello world program
+// #include <iostream>
+// int main()
+// {
+//     std::cout << "Hello Wrld!";
+// }
+
+
 #include <iostream>
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello Wrld!";
+    cout << "Hello world";
+    cout << "C++ will make me an Automotive Software Engineer";
 }
