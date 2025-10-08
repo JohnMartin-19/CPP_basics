49f9040f3e9cf5d480d50e0cbd39fa6c7d2a4509
John Mburu <johnnkonge2020@gmail.com>
2025-10-08 11:27:35 +0300

    worked with variables

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //variable declaration(with its datatype)
+    int a,b;
+    int result;
+
+    //assigning values to vars
+    a= 10; 
+    b=5;
+    result = a*b;
+
+    cout << result;
+    return 0;
+    
+}
