a0ebd3d44837c31cd56365b420dd4ed6eda609dd
John Mburu <johnnkonge2020@gmail.com>
2025-10-08 12:44:34 +0300

    working with strings

---
+++ strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string myString;
+    myString = "I will become an Automotive Software Engineer";
+    cout << myString;
+    return 0;
+}
