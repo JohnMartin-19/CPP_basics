8de939da144178c6128f759f88d08fde35e83ccf
John Mburu <johnnkonge2020@gmail.com>
2025-10-08 14:57:20 +0300

    endl

--- strings.cpp
+++ strings.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main()
 {
     string myString;
+    string myTest;
+    string myAim;
     myString = "I will become an Automotive Software Engineer";
-    cout << myString;
+    myTest="I will make it out the block!";
+    myAim="Imma be the richest in my family!";
+
+    cout << myString << endl;
+    cout << myTest << endl;
+    cout << myAim << endl;
+
     return 0;
 }
 
