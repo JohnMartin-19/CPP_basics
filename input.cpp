d1d8a18899abddafe64ff636ee03423f00fa6fc5
John Mburu <johnnkonge2020@gmail.com>
2025-10-11 07:20:30 +0300

    new lines in cpp and inputs

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    cout << "Enter an integer: ";
+    cin >> i ;
+    cout << "The value for your int is:" << i ;
+    cout << "And its double is: " << i*2 << "\n";
+}
